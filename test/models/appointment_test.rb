73ea98412e0fb1d6b81de09d1ce8691ed9ef1039
Andrew McIndoe <amcindoe@powerhrg.com>
2021-10-26 09:28:28 -0400

    commit tree 5c31c0c4f60c2db1f0e04692805862a87a520eed

--- test/models/appointment_test.rb
+++ test/models/appointment_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
+require 'active_support/core_ext/module/delegation'
 
 class AppointmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+
+  module Shoulda
+    module Matchers
+      module ActiveRecord
+
+        RSpec.describe Appointment, type: :model do
+                it { should belong_to(:trainer) }
+              end
+          
+              # Minitest (Shoulda)
+              class ApplicationTest < ActiveSupport::TestCase
+                should belong_to(:trainer)
+              end
+
+              RSpec.describe Appointment, type: :model do
+                it { should belong_to(:client) }
+              end
+          
+              # Minitest (Shoulda)
+              class ApplicationTest < ActiveSupport::TestCase
+                should belong_to(:client)
+              end
 end
